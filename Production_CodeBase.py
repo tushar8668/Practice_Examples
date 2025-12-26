41cf3261775c7f2cc8650efbc9c0404ddb48212b
Tushar Sutar <tusharsutar27@gmail.com>
2025-12-26 15:35:52 +0530

    Initial Commit

---
+++ Production_CodeBase.py
@@ -0,0 +1,104 @@
+# # Handling outliers.
+
+# def remove_outliers(dataset, cols):
+#     """
+#     Remove outliers from a DataFrame using the interquartile range (IQR) method.
+
+#     Parameters:
+#     data (pandas DataFrame): The DataFrame to remove outliers from.
+#     cols (list): A list of columns to analyze for outliers.
+
+#     Returns:
+#     pandas DataFrame: The DataFrame with outliers removed.
+#     """
+#     # Calculate the IQR for each column
+#     Q1 = dataset[cols].quantile(0.25)
+#     Q3 = dataset[cols].quantile(0.75)
+#     IQR = Q3 - Q1
+
+#     # Determine the lower and upper bounds for outliers
+#     lower_bound = Q1 - 1.5 * IQR
+#     upper_bound = Q3 + 1.5 * IQR
+
+#     # Remove the outliers from the dataset
+#     dataset = dataset[~((dataset[cols] < lower_bound) | (dataset[cols] > upper_bound)).any(axis=1)]
+
+#     return dataset
+
+
+
+# # Create a Vehicle class with instance attributes max_speed and mileage. Then, create an object of this class and print its attributes
+# class Vehicle:
+#     def __init__(self, max_speed, mileage):
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+
+#     def capacity(self, seats):
+#         return f"The capacity of given car is {seats} sit."
+
+
+# v1 = Vehicle(30,12)
+
+
+# # Python program to create a class representing a Circle. Include methods to calculate its area and perimeter. 
+
+# class Circle:
+#     def __init__(self, radius):
+#         self.radius = radius
+
+#     def calculate_area(self):
+#         return f"Area of Circle is {3.14*(self.radius**2)}."
+    
+#     def calculate_perimeter(self):
+#         return f"Perimeter of Circle is {3.14*self.radius*2}"
+    
+# circle1 = Circle(5)
+# print(circle1.calculate_area())
+# print(circle1.calculate_perimeter())
+
+
+# Python program to create a person class. Include attributes like name, country and date of birth. Implement a method to determine the person's age. 
+# from datetime import date
+
+# class Person:
+#     def __init__(self, name, country, date_of_birth):
+#         self.name = name
+#         self.country = country
+#         self.date_of_birth = date_of_birth
+
+#     def calculate_age(self):
+#         today = date.today()
+#         age = today.year - self.date_of_birth.year
+#         if today < date(today.year, self.date_of_birth.month, self.date_of_birth.day):
+#             age -= 1
+#         return f"Persons name is {self.name}. He is leaving in {self.country}, and he is {age} old."    
+        
+# p1 = Person("Tushar", "India", date(2000,1,1))
+# print(p1.calculate_age())
+
+
+# Python program to create a calculator class. Include methods for basic arithmetic operations
+
+class Calculator:
+    def __init__(self, first_number, second_number):
+        self.first_number = first_number
+        self.second_number = second_number
+
+    def addition(self):
+        return f"Addition of {self.first_number} and {self.second_number} is {self.first_number + self.second_number}." 
+
+    def subtraction(self):
+        return f"Subtraction of {self.first_number} and {self.second_number} is {self.first_number - self.second_number}." 
+
+    def multiplication(self):
+        return f"Multiplication of {self.first_number} and {self.second_number} is {self.first_number * self.second_number}." 
+
+    def division(self):
+        return f"Division of {self.first_number} and {self.second_number} is {self.first_number / self.second_number}." 
+    
+c1 = Calculator(12,30)    
+print(c1.addition())
+print(c1.subtraction())
+print(c1.multiplication())
+print(c1.division())
+
