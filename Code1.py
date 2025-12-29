65cc47c445295ea18c17d30485b162a17d46b7f9
Tushar Sutar <tusharsutar27@gmail.com>
2025-12-29 10:02:41 +0530

    New Set - Code1

---
+++ Code1.py
@@ -0,0 +1,21 @@
+# Create a class Employee with attributes name and salary.
+
+# Create two objects of a class Mobile with different brand and price.
+
+# Create a class Book with attributes title, author, and price. Print details using a method.
+
+# Create a class BankAccount with: account holder name balance Add a method to display balance.
+
+# Create a class Employee. Add a method to increase salary by a given amount.
+
+# Create a class ShoppingCart. Add a method to add items and update total price.
+
+# Create a class ATM. Add methods for deposit and withdrawal.
+
+# Create a class MovieTicket. Store movie name, seat number, and price. Add a method to apply discount.
+
+# Create a class OnlineCourse. Store course name and duration. Add a method to update course duration.  
+
+# Create a class Student. Create 3 objects and store them in a list. Print details of all students.
+
+# Create a class Company. Count how many objects are created.
